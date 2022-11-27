cbe6a9ff9a95e4637e12b71525aaf65b25b7438b
Yolvan G. Zambrano <yolvangzg@gmail.com>
2022-11-27 19:38:53 -0400

    Added necessary files for testing (and configuration example)

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "util.h"
+
+using namespace std;
+
+int main () {
+	int entero;
+	float flotante;
+	double doble;
+	util::setConfig(false, true, true);
+
+	cout << "Este es un mensaje" << endl;
+	util::pause();
+	util::borrarPantalla();
+	entero = util::inputNumber<int>("Ingrese un numero entero: ", 20);
+	flotante = util::inputNumber<float>("Ingrese un numero flotante: ");
+	doble = util::inputNumber<double>("Ingrese un numero doble: ", 20.005, 0.0001);
+
+	cout << entero << " + " << flotante << " = " << (entero + flotante) << endl;
+	cout << doble << " - " << flotante << " = " << (doble - flotante) << endl;
+	cout << doble << " * " << 2 << " = " << (doble * 2) << endl;
+	util::pause();
+}
